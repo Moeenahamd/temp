a5198bc1a8b51df0e5ddcd0278d144cfcdf757a1
Moeenahamd <43288441+Moeenahamd@users.noreply.github.com>
2023-10-28 23:40:34 +0500

    Update myFunction.js

--- myFunction.js
+++ myFunction.js
@@ -4,7 +4,7 @@ async function callApi() {
   try {
     const response = await axios.get('https://api.github.com/repos/ARK-Builders/website/issues');
     if (response.status === 200) {
-      console.log(response)
+      console.log(response.data)
       return response.data;
     } else {
       throw new Error(`API request failed with status ${response.status}`);
