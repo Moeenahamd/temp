4eced6781645eb6fede9bec1eed60f12e58ecdbc
Moeenahamd <43288441+Moeenahamd@users.noreply.github.com>
2023-10-29 12:36:34 +0500

    Update myFunction.js

--- myFunction.js
+++ myFunction.js
@@ -1,14 +1,17 @@
 const axios = require('axios');
 
 async function callApi() {
+  var issuesList = [];
   try {
     const response = await axios.get('https://api.github.com/users/Moeenahamd/repos');
     if (response.status === 200) {
       const data = JSON.stringify(response.data);
       const repos = response.data.map(element => element.full_name);
-      
-      console.log(repos);
-      return repos;
+      for (const repo of repos) {
+        issuesList.push(repo)
+      }
+      console.log(issuesList);
+      return issuesList;
     } else {
       throw new Error(`API request failed with status ${response.status}`);
     }
